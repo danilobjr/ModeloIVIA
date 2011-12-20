99ef2d809506ab48674effd54ca7f780a1748a63
Danilo Jr <danilobjr@gmail.com>
2011-12-20 11:40:06 -0300

    criacao do conteudo da HOME e ajustes no design

--- ModeloIVIA.Web/Controllers/HomeController.cs
+++ ModeloIVIA.Web/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ModeloIVIA.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Bem-vindo!";
 
             return View();
         }
